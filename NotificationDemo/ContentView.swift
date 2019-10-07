def9d0858d969f4bd249e7fe5c07e409589e054b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-10-07 22:22:25 +1000

    Initial Commit

---
+++ NotificationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotificationDemo
+//
+//  Created by Lachlan Miller on 7/10/19.
+//  Copyright © 2019 Lachlan Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
