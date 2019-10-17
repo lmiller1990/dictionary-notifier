5d084530c5217170357a8af046ea556c76565e1a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-10-17 22:10:24 +1000

    Add code to check if word is in DB

--- NotificationDemo/Views/ContentView.swift
+++ NotificationDemo/Views/ContentView.swift
@@ -41,57 +41,39 @@ struct ContentView: View {
         }
     }
     
-    func retrieveData() {
-            
-            //As we know that container is set up in the AppDelegates so we need to refer that container.
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-                print("Something bad happened")
-                return
-                
-        }
-            
-            //We need to create a context from this container
-            let managedContext = appDelegate.persistentContainer.viewContext
-            
-            //Prepare the request of type NSFetchRequest  for the entity
-            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "QueuedNotifications")
-            
-    //        fetchRequest.fetchLimit = 1
-    //        fetchRequest.predicate = NSPredicate(format: "username = %@", "Ankur")
-    //        fetchRequest.sortDescriptors = [NSSortDescriptor.init(key: "email", ascending: false)]
-    //
-            do {
-                let result = try managedContext.fetch(fetchRequest)
-                for data in result as! [NSManagedObject] {
-                    print(data.value(forKey: "uuid") as! String)
-                    print(data.value(forKey: "word") as! String)
-                }
-                
-            } catch {
-                
-                print("Failed")
-            }
-        }
-        
-    
     init() {
         print("Initializing")
-        // createData()
-        retrieveData()
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "QueuedNotifications")
+        
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "uuid") as! String)
+                print(data.value(forKey: "word") as! String)
+            }
+        } catch {
+            print("Failed")
+        }
     }
     
     func parseRawJishoResponse(response: RawJishoResponse) -> [Entry] {
         
         return response.data.reduce([]) {(acc: [Entry], curr: RawJishoResponse.Entry) -> [Entry] in
             if curr.japanese.count > 0 && curr.japanese[0].reading != nil && curr.senses.count > 0 {
-                return acc + [Entry(
-                    kana: curr.japanese[0].reading!,
-                    kanji: curr.japanese[0].word != nil ? curr.japanese[0].word : nil,
-                    meaning: Entry.Meaning(
-                        definitions: curr.senses[0].english_definitions,
-                        partsOfSpeech: curr.senses[0].parts_of_speech
+                return acc + [
+                    Entry(
+                        kana: curr.japanese[0].reading!,
+                        kanji: curr.japanese[0].word != nil ? curr.japanese[0].word : nil,
+                        meaning: Entry.Meaning(
+                            definitions: curr.senses[0].english_definitions,
+                            partsOfSpeech: curr.senses[0].parts_of_speech)
                     )
-                    )]
+                ]
             }
             
             return acc
@@ -99,25 +81,15 @@ struct ContentView: View {
     }
     
     func constructNotificationText(_ entry: Entry) -> String {
-        var text = ""
-        if (entry.kanji != nil) {
-            text = entry.kanji!
-        } else {
-            text = entry.kana
-        }
+        var text = getWord(entry)
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return ""
         }
         
-        //We need to create a context from this container
         let managedContext = appDelegate.persistentContainer.viewContext
         
-        //Now let’s create an entity and new user records.
         let userEntity = NSEntityDescription.entity(forEntityName: "QueuedNotifications", in: managedContext)!
-        
-        //final, we need to add some data to our newly created record for each keys using
-        //here adding 5 data with loop
         
         let user = NSManagedObject(entity: userEntity, insertInto: managedContext)
         user.setValue(text, forKey: "word")
@@ -133,8 +105,61 @@ struct ContentView: View {
         return text
     }
     
+    func getWord(_ entry:  Entry) -> String {
+        if (entry.kanji != nil) {
+            return entry.kanji!
+        }
+        
+        return entry.kana
+    }
+    
+    func checkDb(_ entry: Entry) -> Bool {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            print("Something bad happened")
+            return false
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let word = getWord(entry)
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "QueuedNotifications")
+        fetchRequest.predicate = NSPredicate(format: "word == %@", word)
+        
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            return result.count > 0
+        } catch {
+            print("Failed")
+            return false
+        }
+    }
+    
+    func addWordToDb(_ entry: Entry) {
+        let text = getWord(entry)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let userEntity = NSEntityDescription.entity(forEntityName: "QueuedNotifications", in: managedContext)!
+        
+        let user = NSManagedObject(entity: userEntity, insertInto: managedContext)
+        user.setValue(text, forKey: "word")
+        user.setValue(UUID().uuidString, forKey: "uuid")
+    }
+    
     func handleNotification(entry: Entry) {
+        if checkDb(entry) {
+            print("Word in DB. Do nothing.")
+            return
+        }
+        print("Adding word to DB.")
+        addWordToDb(entry)
+        
         let text = constructNotificationText(entry)
+        addWordToDb(entry)
         self.manager.addNotification(title: text)
         self.manager.schedule()
     }
